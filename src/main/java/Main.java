5a335e1d9d0a2993c26683a2b48b2342f02fa487
NikoP93 <nipe0001@stud.kea.dk>
2023-09-25 13:15:12 +0200

    Third commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Kom nuuuu");
         System.out.println("Its alive");
+        System.out.println("Bla");
+        System.out.println("Bla2");
+
     }
 }
