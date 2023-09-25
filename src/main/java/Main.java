977a745a926245af681b4b56b5a8518a84c73861
UsmanSharif0001 <ussh0001@kea.stud.dk>
2023-09-25 13:20:42 +0200

    Test4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Kom nuuuu");
         System.out.println("Its alive");
+        System.out.println("Merge & Conquer");
+        System.out.println("Merge & Conquer2");
         System.out.println("Bla");
         System.out.println("Bla2");
         System.out.println("Fedt mand");
+        System.out.println("Bla3");
 
     }
 }
